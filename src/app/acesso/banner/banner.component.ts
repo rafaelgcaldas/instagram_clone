75115b669931477f09a939a5d45fd5840d58cb23
Your Name <rafaelzinhogomes22@live.com>
2018-10-05 14:44:40 -0300

    Animando as imagens do componente banner

--- src/app/acesso/banner/banner.component.ts
+++ src/app/acesso/banner/banner.component.ts
@@ -1,15 +1,60 @@
 import { Component, OnInit } from '@angular/core';
+import { trigger, state, style, transition, animate } from '@angular/animations';
+import { Imagem } from './imagem.model'
 
 @Component({
   selector: 'app-banner',
   templateUrl: './banner.component.html',
-  styleUrls: ['./banner.component.css']
+  styleUrls: ['./banner.component.css'],
+  animations: [
+    trigger('banner', [
+      state('escondido', style({
+        opacity: 0
+      })),
+      state('visivel', style({
+        opacity: 1
+      })),
+      transition('escondido <=> visivel', animate('0.5s ease-in'))
+    ])
+  ]
 })
 export class BannerComponent implements OnInit {
+
+  public estado: string = "escondido"
+  public imagens: Imagem[] = [
+    { estado: "visivel", url: "/assets/banner-acesso/img_1.png" },
+    { estado: "escondido", url: "/assets/banner-acesso/img_2.png" },
+    { estado: "escondido", url: "/assets/banner-acesso/img_3.png" },
+    { estado: "escondido", url: "/assets/banner-acesso/img_4.png" },
+    { estado: "escondido", url: "/assets/banner-acesso/img_5.png" }
+  ]
 
   constructor() { }
 
   ngOnInit() {
+    setTimeout(() => this.logicaRotacao(), 3000);
+  }
+
+  public logicaRotacao(): void {
+    
+    let index: number
+
+    // ocultar imagem
+    for (let i:number = 0; i <= 4; i++) {
+      
+      if(this.imagens[i].estado === "visivel"){
+        this.imagens[i].estado = 'escondido'
+        
+        index = i === 4 ? 0 : i + 1
+        break
+      }
+      
+    }
+
+    // exibir a próxima imagem
+    this.imagens[index].estado = 'visivel'
+
+    setTimeout(() => this.logicaRotacao(), 3000);
   }
 
 }
