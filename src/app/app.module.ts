fb54ea252f41a317c2ce74a382ecd57fe21c91d9
Your Name <rafaelzinhogomes22@live.com>
2018-10-06 14:13:06 -0300

    Configurando rotas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule} from '@angular/platform-browser/animations'
 import { ReactiveFormsModule } from '@angular/forms'
+import { RouterModule } from '@angular/router'
 
+import { ROUTES } from './app.routes'
 import { AppComponent } from './app.component';
 import { AcessoComponent } from './acesso/acesso.component';
 import { BannerComponent } from './acesso/banner/banner.component';
@@ -25,7 +27,8 @@ import { PublicacoesComponent } from './home/publicacoes/publicacoes.component';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    RouterModule.forRoot(ROUTES)
   ],
   providers: [Auth],
   bootstrap: [AppComponent]
