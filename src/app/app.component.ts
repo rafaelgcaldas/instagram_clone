8e980296d0bda27d8de03b61b1be2f12cb5505e4
Your Name <rafaelzinhogomes22@live.com>
2018-10-06 00:26:17 -0300

    Conectando o firebase SDK do projeto com o back-end do Firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as firebase from 'firebase'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app3';
+export class AppComponent implements OnInit {
+  
+  ngOnInit(): void {
+    let config = {
+      apiKey: "",
+      authDomain: "",
+      databaseURL: "",
+      projectId: "",
+      storageBucket: "",
+      messagingSenderId: ""
+    };
+    firebase.initializeApp(config)
+  }
 }
