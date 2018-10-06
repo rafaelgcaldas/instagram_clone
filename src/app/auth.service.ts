eaff782313d43cd35b202e1cfe3c098e441e492a
Your Name <rafaelzinhogomes22@live.com>
2018-10-06 11:56:29 -0300

    Cadastrando usuarios no firebase

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -1,7 +1,21 @@
 import { Usuario } from "./acesso/usuario.model";
+import * as firebase from 'firebase'
 
 export class Auth {
     public cadastrarUsuario(usuario: Usuario): void {
         console.log(usuario)
+
+        firebase.auth().createUserWithEmailAndPassword(usuario.email, usuario.senha)
+            .then((resposta: any) => {
+                // removendo o atributo senha do objeto usuario
+                delete usuario.senha
+
+                // registrando dados complementares do usuario no path email na base64
+                firebase.database().ref(`usuario_detalhe/${btoa(usuario.email)}`)
+                    .set( usuario )
+            })
+            .catch((error: Error) => {
+                console.log(error)
+            })
     }
 }
