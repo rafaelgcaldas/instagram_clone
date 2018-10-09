e2801ccad70c89b2a3d70f9d80bc1172696722fa
Your Name <rafaelzinhogomes22@live.com>
2018-10-08 21:54:13 -0300

    Ajustando a gravação da imagem e título da publicação

--- src/app/bd.service.ts
+++ src/app/bd.service.ts
@@ -8,25 +8,32 @@ export class Bd {
     constructor(private progressoService: Progresso){}
 
     public publicar(publicacao: any): void {
-        // firebase.database().ref(`publicacoes/${btoa(publicacao.email)}`)
-        //     .push({ titulo: publicacao.titulo })
 
-        let nomeImagem = Date.now()
+        
 
-        firebase.storage().ref()
-            .child(`imagens/${nomeImagem}`)
-            .put(publicacao.imagem)
-            .on(firebase.storage.TaskEvent.STATE_CHANGED,
-                (snapshot: any) => { // Acompanhamento do progresso o Upload
-                    this.progressoService.status = "andamento"
-                    this.progressoService.estado = snapshot
-                }, 
-                (error) => { 
-                    this.progressoService.status = "erro"
-                },
-                () => {  //Fianlização do processo
-                    this.progressoService.status = "concluido"
-                }
-            )
+        firebase.database().ref(`publicacoes/${btoa(publicacao.email)}`)
+            .push({ titulo: publicacao.titulo })
+            .then((resposta: any) => {
+
+                let nomeImagem = resposta.key
+                
+                firebase.storage().ref()
+                    .child(`imagens/${nomeImagem}`)
+                    .put(publicacao.imagem)
+                    .on(firebase.storage.TaskEvent.STATE_CHANGED,
+                        (snapshot: any) => { // Acompanhamento do progresso o Upload
+                            this.progressoService.status = "andamento"
+                            this.progressoService.estado = snapshot
+                        }, 
+                        (error) => { 
+                            this.progressoService.status = "erro"
+                        },
+                        () => {  //Finalização do processo
+                            this.progressoService.status = "concluido"
+                        }
+                    )
+            })
+
+        
     }
 }
